5245ba059742bbf28b5d01f774a51d45329197d4
Nikki Bruls <Nikki.Bruls@student.pxl.be>
2021-04-15 08:59:40 +0000

    constants

--- stern4most_lidar_AI2/scripts/stern4most_lidar_AI2.py
+++ stern4most_lidar_AI2/scripts/stern4most_lidar_AI2.py
@@ -7,7 +7,12 @@ import sys
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-ANGLE = 360
+SPEED = 0.27
+MAX_DISTANCE = 0.65
+MIN_ANGLE_LEFT = 0
+MAX_ANGLE_LEFT = 40
+MIN_ANGLE_RIGHT = 310
+MAX_ANGLE_RIGHT = 360
 
 
 class LaserListener():
@@ -25,7 +30,7 @@ class LaserListener():
         # The publisher for the lidar_controller topic will publish Twist messages with a static linear.x value of 0.27. 
         # The rate is set to 10.
         self.vel = Twist()
-        self.vel.linear.x = 0.27
+        self.vel.linear.x = SPEED
         self.rate = rospy.Rate(10)
 
     def callback_scan(self, data):
@@ -40,18 +45,18 @@ class LaserListener():
         weighted_angle = 0
         object_found = False
         for distance in data.ranges:
-            if distance < 0.65:
+            if distance < MAX_DISTANCE:
                 if object_found == False:
                     object_found = True
                 # look at every degree around the robot
                 current_angle = data.angle_min + (data.angle_increment * index)
                 current_angle = math.degrees(current_angle)
 
-                if current_angle < 40:
-                    weighted_angle = current_angle
+                if MIN_LEFT_ANGLE < current_angle < MAX_LEFT_ANGLE:
+                    weighted_angle = MAX_LEFT_ANGLE - current_angle
                     left_weighted_values.append(weighted_angle / distance)
-                elif current_angle > 310:
-                    weighted_angle = ANGLE - current_angle
+                elif MIN_LEFT_ANGLE < current_angle < MAX_RIGHT_ANGLE:
+                    weighted_angle = MAX_RIGHT_ANGLE - current_angle
                     right_weighted_values.append(weighted_angle / distance)
             index += 1
         avgL = sum(left_weighted_values)/len(left_weighted_values) if len(left_weighted_values) != 0 else 0
