01017263bc5d0cf8ae048d0921cea3258ddfc16e
timmermansjoy <61321383+timmermansjoy@users.noreply.github.com>
2021-04-19 18:25:43 +0200

    Added Machine learning moddel based on first 3 corners
    Co-authored-by: StijnJacobsPXL <StijnJacobsPXL@users.noreply.github.com>
    Co-authored-by: Nikki Bruls <NikkiBrulsPXL@users.noreply.github.com>

--- stern4most_AI_AI2/scripts/stern4most_AI_AI2.py
+++ stern4most_AI_AI2/scripts/stern4most_AI_AI2.py
@@ -2,7 +2,9 @@
 
 import rospy
 import cv2
+import numpy as np
 import math
+from tensorflow.keras.models import load_model
 from cv_bridge import CvBridge, CvBridgeError
 from time import sleep
 
@@ -11,71 +13,37 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Bool
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import LaserScan
-
-import data_collection
 
 
-maxThrottle = 0.3
-record = 0
-GUI_UPDATE_PERIOD = 0.10  # Seconds
-
-
-class AI:
+class AI_driver:
     def __init__(self):
-        # Initial values
-        self.running = True
-        self.bridge = CvBridge()
-        self.rate = rospy.Rate(10)
+        self.steeringSen = 1.2
         self.image = None
         self.imageLock = Lock()
-        self.lidar_message = Twist()
-        self.STEERING = Twist()
-        self.RECORD = False
-        self.recording_step = 0
-        self.statusMessage = ''
-        self.connected = False
-        self.redrawTimer = rospy.Timer(rospy.Duration(GUI_UPDATE_PERIOD), self.collect_data_and_save)
+        self.bridge = CvBridge()
+        self.model = load_model('/home/user/Projects/catkin_ws/src/ROS_Robot/stern4most_AI_AI2/model.h5')
 
         # ---- Subscribers ----
-        self.video_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.callback_image_raw)
+        self.camera_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.callback_image_raw)
         rospy.loginfo('subscribed to topic /camera/rgb/image_raw')
 
-        self.lidar_sub = rospy.Subscriber("/scan", LaserScan, self.callback_lidar_scan)
-        rospy.loginfo("Subscribed to topic /scan")
+        # ---- Publishers ----
+        self.autonomous_controller_pub = rospy.Publisher('autonomous_controller', Twist, queue_size=10)
+        rospy.loginfo('created publisher for topic autonomous_controller')
 
-        self.record_sub = rospy.Subscriber('record', Bool, self.callback_record)
-        rospy.loginfo('subscribed to topic record')
+        self.vel = Twist()
+        self.vel.linear.x = 0.22
 
-        self.turning_sub = rospy.Subscriber('manual_controller', Twist, self.callback_steering)
-        rospy.loginfo('subscribed to topic manual_controller')
+     # ---- Callbacks ----
 
-    # ---- Callbacks ----
     def callback_image_raw(self, data):
         self.imageLock.acquire()
         try:
             self.image = data
         finally:
             self.imageLock.release()
+        self.main()
 
-    def callback_lidar_scan(self, data):
-        data_perSweep = []
-        index = 0
-        object_found = False
-        for value in data.ranges:
-            current_angle = data.angle_min + (data.angle_increment * index)
-            values = [current_angle, value]
-            data_perSweep.append(values)
-            index += 1
-
-    def callback_record(self, data):
-        if data.data == True:
-            self.RECORD = not self.RECORD
-
-    def callback_steering(self, data):
-        self.STEERING = data
-
-    # ---- Helpers ----
     def convert_ros_to_opencv(self, ros_image):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
@@ -83,33 +51,31 @@ class AI:
         except CvBridgeError as error:
             raise Exception("Failed to convert to OpenCV image")
 
-    def collect_data_and_save(self, event):
-        # Preprocess the image
-        if self.running == True and self.image is not None:
-            self.imageLock.acquire()
-            try:
-                # Convert the captured frame from ROS to OpenCV.
-                image_cv = self.convert_ros_to_opencv(self.image)
-            finally:
-                self.imageLock.release()
-            image_cv = cv2.resize(image_cv, dsize=(228, 156), interpolation=cv2.INTER_CUBIC)
+    def preProcess(self, img):
+        img = img[54:120, :, :]
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        img = cv2.GaussianBlur(img, (3, 3), 0)
+        img = cv2.resize(img, (200, 66))
+        img = img / 255
+        return img
 
-        if self.RECORD:  # later change to (if recording button is pressed)
-            if self.recording_step == 0:
-                rospy.loginfo('Recording ...')
-                self.recording_step += 1
-            sleep(0.300)
-            if self.recording_step == 1:
-                data_collection.saveData(image_cv, self.STEERING.angular.z)
-        if not self.RECORD and self.recording_step == 1:
-            data_collection.saveLog()
-            self.recording_step = 0
+    def main(self):
+        if self.image is not None:
+            img = self.convert_ros_to_opencv(self.image)
+            img = cv2.resize(img, dsize=(228, 156), interpolation=cv2.INTER_CUBIC)
+            img = np.asarray(img)
+            img = self.preProcess(img)
+            img = np.array([img])
 
-        # motor.move(throttle, -steering)
+            steering = float(self.model.predict(img))
+            print(steering * self.steeringSen)
+
+            self.vel.angular.z = steering * self.steeringSen
+            self.autonomous_controller_pub.publish(self.vel)
 
 
 if __name__ == "__main__":
-
-    rospy.init_node("AI_listener")
-    AI()
+    rospy.init_node("AI_driver")
+    rospy.loginfo('AI_driver node has been initiated')
+    AI_driver()
     rospy.spin()
--- training/utils.py
+++ training/utils.py
@@ -1,31 +1,34 @@
 import os
 import pandas as pd
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 import cv2
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Convolution2D, Flatten, Dense 
-from tensorflow.keras.optimizers import Adam 
+from tensorflow.keras.layers import Convolution2D, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
 
-import matplotlib.image as mpimg 
-from imgaug import augmenters as iaaa
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
 
-import random 
+import random
 
-# STEP 1 - Initialisation of the data 
+# STEP 1 - Initialisation of the data
+
+
 def getName(filePath):
     imgPathL = filePath.split('/')[-2:]
     imgPath = os.path.join(imgPathL[0], imgPathL[1])
-    return imgPath    
+    return imgPath
+
 
 def importDataInfo(path):
     columns = ['Center', 'Steering']
     numberOfFolders = len(os.listdir(path))//2
     data = pd.DataFrame()
-    for x in range(4):
-        newData = pd.read_csv(os.path.join(path, f'log_{x}.csv'), names = columns)
+    for x in range(2):
+        newData = pd.read_csv(os.path.join(path, f'log_{x}.csv'), names=columns)
         newData['Center'] = newData['Center'].apply(getName)
         data = data.append(newData, True)
     print(' ')
@@ -33,10 +36,12 @@ def importDataInfo(path):
     return data
 
 
-# STEP 2 - VISUALIZE AND BALANCE DATA
-def balanceData(data,display=True):
+# STEP 2 - Visualize and balance the data
+
+
+def balanceData(data, display=True):
     nBin = 31
-    samplesPerBin =  300
+    samplesPerBin = 1000
     hist, bins = np.histogram(data['Steering'], nBin)
     if display:
         center = (bins[:-1] + bins[1:]) * 0.5
@@ -68,16 +73,96 @@ def balanceData(data,display=True):
         plt.ylabel('No of Samples')
         plt.show()
     return data
-    
-#### STEP 3 - PREPARE FOR PROCESSING
+
+# STEP 3 - Prepare for data processing
+
+
 def loadData(path, data):
     print(data)
     imagesPath = []
     steering = []
     for i in range(len(data)):
         indexed_data = data.iloc[i]
-        imagesPath.append( os.path.join(path,indexed_data[0]))
-        steering.append(float(indexed_data[1]))
+        filepath = os.path.join(path, indexed_data[0])
+        if os.path.isfile(filepath):
+            imagesPath.append(os.path.join(path, indexed_data[0]))
+            steering.append(float(indexed_data[1]))
     imagesPath = np.asarray(imagesPath)
     steering = np.asarray(steering)
     return imagesPath, steering
+
+# STEP 5 - Augment the data
+
+
+def augmentImage(imgPath, steering):
+    img = mpimg.imread(imgPath)
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
+        img = pan.augment_image(img)
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1, 1.2))
+        img = zoom.augment_image(img)
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+        steering = -steering
+    return img, steering
+
+
+# imgRe, st = augmentImage('drivingData/IMG0/Image_16187651324266.jpg', 0)
+# plt.imshow(imgRe)
+# plt.show()
+
+
+# STEP 6 - Preprocessing (Nvidia recomended)
+def preProcessing(img):
+    img = img[54:120, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+#imgRe = preProcessing(mpimg.imread('drivingData/IMG0/Image_16187651324266.jpg'))
+# plt.imshow(imgRe)
+# splt.show()
+
+
+# STEP 7 - CREATE MODEL
+def createModel():
+    model = Sequential()
+
+    model.add(Convolution2D(24, (5, 5), (2, 2), input_shape=(66, 200, 3), activation='elu'))
+    model.add(Convolution2D(36, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(48, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
+
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
+
+    model.compile(Adam(lr=0.0001), loss='mse')
+    return model
+
+# STEP 8 - Training
+
+
+def dataGen(imagesPath, steeringList, batchSize, trainFlag):
+    while True:
+        imgBatch = []
+        steeringBatch = []
+
+        for i in range(batchSize):
+            index = random.randint(0, len(imagesPath) - 1)
+            if trainFlag:
+                img, steering = augmentImage(imagesPath[index], steeringList[index])
+            else:
+                img = mpimg.imread(imagesPath[index])
+                steering = steeringList[index]
+            img = preProcessing(img)
+            imgBatch.append(img)
+            steeringBatch.append(steering)
+        yield(np.asarray(imgBatch), np.asarray(steeringBatch))
