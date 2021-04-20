b253f03228007766f228d33f1f9a282dd2cf6d66
Nikki Bruls <Nikki.Bruls@student.pxl.be>
2021-04-20 15:02:56 +0000

    fixed bug in communication for backwards driving + code cleanup

--- stern4most_communication_AI2/scripts/stern4most_communication_AI2.py
+++ stern4most_communication_AI2/scripts/stern4most_communication_AI2.py
@@ -9,11 +9,11 @@ player_name = "AI2"
 
 class stern4most_communication_AI2:
     def __init__(self):
-        self.manual_autonomous_pub = rospy.Publisher('manual_autonomous', Bool, queue_size=10)
-        rospy.loginfo('created publisher for topic manual_autonomous')
-
+        
+        # ---- Subscribers ----
         self.game_on_subscriber = rospy.Subscriber('game_on', String, self.callback_game_on)
         rospy.loginfo('subscribed to topic game_on')
+        self.start_message = Bool()
 
         self.sector_crossed_sub = rospy.Subscriber('sector_crossed', Bool, self.callback_sector_crossed)
         rospy.loginfo('subscribed to topic sector_info')
@@ -21,31 +21,47 @@ class stern4most_communication_AI2:
         self.ranking_sub = rospy.Subscriber('ranking', String, self.callback_ranking)
         rospy.loginfo('subscribed to topic ranking')
 
+        self.sternformost_sub = rospy.Subscriber('sterformost', Bool, self.callback_sternformost)
+        rospy.loginfo('subscribed to topic sternformost')
+        self.BACKWARDS = False
+
+        # ---- Publishers ----
+        self.manual_autonomous_pub = rospy.Publisher('manual_autonomous', Bool, queue_size=10)
+        rospy.loginfo('created publisher for topic manual_autonomous')
+
         self.ranking_pub = rospy.Publisher('dashboard_ranking', String, queue_size=10)
         rospy.loginfo('created publisher for topic dashboard_ranking')
 
-        self.sector = 1
-        self.start_message = Bool()
+        # ---- Service ----
         self.sector_update = rospy.ServiceProxy('status_update', SectorUpdate)
+
+        self.sector = 0 if self.BACKWARDS else 1
+        
 
     def callback_ranking(self, msg):
         self.ranking_pub.publish(msg)
 
     def callback_game_on(self, msg):
-        rospy.loginfo('received message from referee: ' + str(msg.data))
         if msg.data == 'Start':
-            rospy.loginfo('publishing to topic manual_autonomous')
             self.start_message.data = True
+            rospy.loginfo('publishing to topic manual_autonomous with value ' + str(self.start_message.data))
             self.manual_autonomous_pub.publish(self.start_message)
+
+            rospy.loginfo('Calling sector_update')
             self.sector_update(player_name, int(self.sector))
 
     def callback_sector_crossed(self, msg):
         self.sector = self.sector % 16 + 1
-        self.sector = 1 if self.sector == 0 else self.sector
+        if self.sector == 0:
+            self.sector += 1
+
         response = self.sector_update(player_name, int(self.sector))
         if response.status == 'WRONG_SECTOR' or response.status == 'FINISHED':
             self.start_message.data = False
             self.manual_autonomous_pub.publish(self.start_message)
+    
+    def callback_sternformost(self, msg):
+        self.BACKWARDS = msg.data
 
 
 if __name__ == '__main__':
